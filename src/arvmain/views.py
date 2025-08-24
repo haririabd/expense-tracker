1a24704febdddebb2b2ecd2e1a62ee34cce218a6
Hariri A. <110659046+haririabd@users.noreply.github.com>
2025-08-24 09:34:19 +0000

    Initial commit

---
+++ src/arvmain/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index_view(request, *args, **kwargs):
+    page_title = 'Homepage'
+    html_template = 'landingpage/page/index.html'
+
+    context = {
+        "page_title": page_title,
+    }
+    return render(request, html_template, context)
+
+def about_view(request, *args, **kwargs):
+    page_title = 'About'
+    html_template = 'landingpage/page/index.html'
+
+    context = {
+        "page_title": page_title,
+    }
+    return render(request, html_template, context)
