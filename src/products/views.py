f0df5e571f780a2b104acb983d3fde7df314ab7a
Hariri A. <hariri.bin.abdullah@gmail.com>
2025-08-28 14:46:59 +0800

    refactor views

--- src/products/views.py
+++ src/products/views.py
@@ -15,6 +15,19 @@ class BaseListView(ListView):
         context['owner'] = self.owner
         return context
 
+class BaseCreateView(CreateView):
+    template_name = 'dashboard/page/single_data_form.html'
+    
+    # We'll set these properties in the subclasses
+    page_title = ''
+    owner = ''
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = self.page_title
+        context['owner'] = self.owner
+        return context
+
 class BaseUpdateView(UpdateView):
     fields = ['name']
     template_name = 'dashboard/page/single_data_form.html'
@@ -27,23 +40,19 @@ class BaseUpdateView(UpdateView):
         context['owner'] = self.owner
         return context
 
+# Category Views
 class CategoryListView(BaseListView):
     model = Category
     context_object_name = 'categories'
     page_title = 'Category List'
     owner = 'category'
 
-class CategoryCreateView(CreateView):
+class CategoryCreateView(BaseCreateView):
     model = Category
-    form_class = CategoryForm # Link with form we created in forms.py so we can edit the style
-    template_name = 'dashboard/page/single_data_form.html'
+    form_class = CategoryForm
     success_url = reverse_lazy('categories')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_title'] = 'Add Category'
-        context['owner'] = 'category'
-        return context
+    page_title = 'Add Category'
+    owner = 'category'
     
 class CategoryUpdateView(BaseUpdateView):
     model = Category
@@ -68,17 +77,12 @@ class AuthorListView(BaseListView):
     page_title = 'Author List'
     owner = 'author'
     
-class AuthorCreateView(CreateView):
+class AuthorCreateView(BaseCreateView):
     model = Author
-    form_class = AuthorForm # Link with form we created in forms.py so we can edit the style
-    template_name = 'dashboard/page/single_data_form.html'
+    form_class = AuthorForm
     success_url = reverse_lazy('authors')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_title'] = 'Add Author'
-        context['owner'] = 'author'
-        return context
+    page_title = 'Add Author'
+    owner = 'author'
     
 class AuthorUpdateView(BaseUpdateView):
     model = Author
